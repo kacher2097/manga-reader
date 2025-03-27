e55f0b110392e1811184b90fc02746852f60121f
kacher2097 <ltuan997bg@gmail.com>
2025-03-27 16:06:48 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Icons from 'unplugin-icons/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    Icons({
-      compiler: 'vue3',
-      autoInstall: true,
-    }),
+    tailwindcss(),
   ],
-  css: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
-  },
 })
